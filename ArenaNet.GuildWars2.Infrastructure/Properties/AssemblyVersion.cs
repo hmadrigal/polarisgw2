bcdb6d935da1acc0b0ab2b8f8be77ed0a2785b19
Herber Madrigal <hmadrigal@gmail.com>
2013-09-27 10:36:57 -0600

    Release 1.0.0.4

--- ArenaNet.GuildWars2.Infrastructure/Properties/AssemblyVersion.cs
+++ ArenaNet.GuildWars2.Infrastructure/Properties/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.0.3")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.4")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Gw2WebClient 2013")]
